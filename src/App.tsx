145a6b5e8ec4a9c136775e13f10fc4ff1edc8a5c
Pex-Dev <brayanc.moya@gmail.com>
2025-10-08 16:07:34 -0300

    Hero component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Header from "./components/Layout/Header";
-
+import Hero from "./components/Layout/Hero";
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
