606091506ae9808f87ac0712dee618c0a07495d5
Pex-Dev <brayanc.moya@gmail.com>
2025-10-16 15:09:40 -0300

    Added Advanced Statistics sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import Header from "./components/Layout/Header";
 import Hero from "./components/Layout/Hero";
+import AdvancedStatistics from "./components/Layout/AdvancedStatistics";
+import URLShortering from "./components/Layout/URLShortering";
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <div className="bg-statistics mt-40">
+        <URLShortering />
+        <AdvancedStatistics />
+      </div>
     </>
   );
 }
